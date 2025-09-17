fe0a931d5365a499a41500c3ac981debdaf42b32
harshyadav152 <harshyadav.personal01@gmail.com>
2025-09-17 14:32:40 +0530

    private folder starting from _

---
+++ app/_lib/page.js
@@ -0,0 +1,8 @@
+export default function Library() {
+    return (
+        <>
+            <h2>This is not available in route even using page.js</h2>
+            <h1>However if want to use _ in url use %5F in folder path</h1>
+        </>
+    )
+}
---
+++ app/_lib/utility-functions.js
@@ -0,0 +1,5 @@
+export default function Date(){
+    return <h2>
+        This is not available in route even using page.js
+    </h2>
+}
