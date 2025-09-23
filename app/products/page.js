60f9d10dd13ad1baa0b436887f56446f7eec4c93
harshyadav152 <harshyadav.personal01@gmail.com>
2025-09-23 12:52:56 +0530

    Link navigation in next

--- app/products/page.js
+++ app/products/page.js
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 export default function Products() {
+    const productId = 2;
     return (
         <>
             <h2>Products listing</h2>
             <li>One</li>
-            <li>Two</li>
+            <li>
+                <Link href={`/products/${productId}`} replace> // replace removes link history even it create new url history in stack
+                    Two
+                </Link>
+            </li>
             <li>Three</li>
+            <Link href="/">Home</Link>
         </>
     )
 }
--- app/profile/page.js
+++ app/profile/page.js
@@ -1,9 +1,18 @@
+import Link from "next/link"
+
 export const metadata = {
-    title:{
-        absolute:"Profile" // absolute title which override parent component title
+    title: {
+        absolute: "Profile" // absolute title which override parent component title
     }
 }
 
-export default function Profile(){
-    return <h2>Profile page using file based routing</h2>
+export default function Profile() {
+    return (
+        <>
+            <h2>Profile page using file based routing</h2>
+            <Link href="/">Go to home</Link>
+            <br></br>
+            <Link href="/products">products</Link>
+        </>
+    )
 }
